3ca125b6330c69d64fdb45fb7d8e690a4e1b3e64
John Ramsden <john@ubc.ramsdenj.ca>
2019-09-21 17:47:07 -0700

    Setup MongoDB client and example endpoint

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const path = require('path');
 const logger = require('morgan');
 
 const apiRouter = require('./routes/api/v1.0');
@@ -9,6 +8,7 @@ const app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.use('/api/v1.0', apiRouter);
 
--- routes/api/v1.0/index.js
+++ routes/api/v1.0/index.js
@@ -1,9 +1,39 @@
 const express = require('express');
 const router = express.Router();
 
+const assert = require('assert');
+const config = require('../../../config.js');
+const database = require('../../../database');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.status(200).json({ title: 'API' });
+  res.json({ "title": 'API' });
+});
+
+/* PUT MongoDB. */
+router.post('/testdb', function (req, res, next) {
+  console.log(req.body);
+
+  database.client.connect(function(err) {
+    assert.ok(null === err);
+    const db = database.client.db(config.mongoDBConnection.database);
+
+    insertDocuments(db, req.body, function() {
+      database.client.close();
+    });
+  });
+
+  const insertDocuments = function(db, data, callback) {
+    // Get the documents collection
+    const collection = db.collection('documents');
+    // Insert document
+    collection.insertOne(data, function(err, result) {
+      assert.ok(err === null);
+      console.log("Inserted document into the collection");
+      callback(result);
+    });
+  };
+  res.end();
 });
 
 module.exports = router;
