38eed6c204979c32de2d978d0da81f037e8175a8
John Ramsden <john@ubc.ramsdenj.ca>
2019-10-20 16:35:54 -0700

    Add code to get test token

--- routes/api/v1.0/auth.js
+++ routes/api/v1.0/auth.js
@@ -1,6 +1,35 @@
 var req = require('request');
 
 function routes (fastify, opts, done) {
+    /* GET testing token. */
+    fastify.route({
+        method: 'GET',
+        url: '/google/callback',
+        handler: function(request, reply) {
+            this.getAccessTokenFromAuthorizationCodeFlow(request, (err, result) => {
+                if (err) {
+                    reply.send(err);
+                    return
+                }
+
+                req({
+                    url: 'https://openidconnect.googleapis.com/v1/userinfo',
+                    method: 'GET',
+                    qs: { scope: "openid email"},
+                    headers: {
+                        Authorization: 'Bearer ' + result.access_token
+                    },
+                    json: true
+                }, function (err, res, data) {
+                    if (err) {
+                        reply.send(err);
+                        return
+                    }
+                    reply.send(result)
+                })
+            })
+        }});
+
     /* GET AUTH token. */
     fastify.route({
         method: 'POST',
