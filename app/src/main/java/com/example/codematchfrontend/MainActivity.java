e36e4c7fdd0cc5285b96543938ca68cd14dc2696
Jake Moersch <Jakemoersch@gmail.com>
2019-10-19 19:03:51 -0700

    Began ProfileView and related XML file, continuing work on profileview's buttons

--- app/src/main/java/com/example/codematchfrontend/MainActivity.java
+++ app/src/main/java/com/example/codematchfrontend/MainActivity.java
@@ -2,11 +2,23 @@ package com.example.codematchfrontend;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+
+    /** Called when the user taps the notificationviewbutton button */
+    public void switchTabtoNotifyView(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, NotifyView.class);
+        startActivity(intent);
+    }
+        @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
