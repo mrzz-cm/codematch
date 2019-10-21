aa89ca7988d02c94529600360b22d4b60f36f5aa
szszs <shawn.zhou2@gmail.com>
2019-10-20 17:38:41 -0700

    user can post questions and it will link them to a question ID, but no question database yet

--- routes/api/v1.0/questions.js
+++ routes/api/v1.0/questions.js
@@ -21,15 +21,91 @@ function routes (fastify, opts, done) {
             const qm = questionsModule({ mongo: fastify.mongo });
             const um = userModule({ mongo: fastify.mongo });
 
-            um.User.exists(request.body.userId)
-                .then(result => {
-                    if (result) {
-                        qm.createQuestion(request.body);
-                    } else {
-                        reply.status(400);
-                        reply.send(err);
+
+
+            // qm.createQuestion(request.body, function(err, status, q) {
+            //     if (err || (status !== 200) || !q) {
+            //         console.log(q);
+            //         reply.status(status);
+            //         reply.send(err);
+            //         return
+            //     }
+
+            //     // add it to the database
+            //     console.log(q.toJson());
+
+            //     // done
+            //     reply.status(200);
+            //     reply.send('Question posted.');
+            // });
+            var user;
+
+            function updateUserCallback(err, newUserJson) {
+                user = um.User.fromJson(newUserJson);
+                if (err) {
+                    console.log(err);
+                    reply.status(500);
+                    reply.send(err);
+                    return;
+                }
+
+                reply.status(200);
+                reply.send('yes');
+            }
+
+            function createQuestionCallback(err, status, new_question) {
+                if (err || !new_question) {
+                    reply.status(status);
+                    reply.send(err);
+                    return;
+                }
+
+                // set the user's current question
+                user.currentQuestion = new_question.uuid;
+                user.questionsPosted.push(new_question.uuid);
+
+                // update the user
+                user.update({$set: 
+                    {
+                        currentQuestion: new_question.uuid,
+                        questionsPosted: user.questionsPosted
                     }
-                })
+                }, updateUserCallback);
+            }
+
+            function getUserCallback(err, result) {
+                if (err || !result) {
+                    reply.status(500);
+                    reply.send(err);
+                    return;
+                }
+
+                // check that the user can post the question
+                user = um.User.fromJson(result);
+
+                if (user.currentQuestion) {
+                    reply.status(401);
+                    reply.send('Cannot post a question when you are already ' +
+                            'registered to another question!');
+                    return;
+                }
+
+                // create the question
+                qm.createQuestion(request.body, createQuestionCallback);
+            }
+
+            function userExistsCallback(userExists) {
+                if (userExists) {
+                    // get the user
+                    um.User.retrieve(request.body.userId, getUserCallback);
+                } else {
+                    reply.status(400);
+                    reply.send('Provided user doesn\'t exist.');
+                }
+            }
+
+            um.User.exists(request.body.userId)
+                .then(userExistsCallback)
                 .catch(err => {
                     reply.status(400);
                     reply.send(err);
