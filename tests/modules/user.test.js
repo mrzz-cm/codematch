ca427a669e01b9a33548d5cbae2e7fe451bfffa3
John Ramsden <john@ubc.ramsdenj.ca>
2019-11-17 18:26:33 -0800

    Add User update test

--- tests/modules/user.test.js
+++ tests/modules/user.test.js
@@ -9,24 +9,59 @@ const fastify = app.fastify;
 
 let um;
 
-describe("Basic test", () => {
+describe("User creation test", () => {
+    const tu = "testuser@example.com";
+
     beforeEach(async () => {
         await fastify.ready();
         um = userModule({ mongo: fastify.mongo });
     });
 
-    afterAll(() => {
+    afterAll(async () => {
+        const collection = await fastify.mongo.db.collection("users");
+        await collection.deleteOne({userId: tu});
         fastify.close();
     });
 
     test("Checks user is created", async (done) => {
-        const tu = "testuser@example.com";
         const user = um.User.newUser(tu);
+
+        /* Make sure removed in last run */
+        expect(await um.User.exists(tu)).toBeFalsy();
 
         user.create()
             .then(() => um.User.exists(tu))
             .then((result) => expect(result).toBeTruthy())
             .then(() => done());
     });
+});
 
+describe("User update test", () => {
+    const tu = "testuser@example.com";
+    let user;
+
+    beforeEach(async () => {
+        await fastify.ready();
+        um = userModule({ mongo: fastify.mongo });
+        user = um.User.newUser(tu);
+        await user.create();
+    });
+
+    afterAll(async () => {
+        const collection = await fastify.mongo.db.collection("users");
+        await collection.deleteOne({userId: tu});
+        fastify.close();
+    });
+
+    test("Checks user is updated", async (done) => {
+        const courses = [ "CPEN321" ];
+
+        /* Make sure created properly */
+        expect(user.toJson().userId).toBe(tu);
+
+        user.update({ $set: { courses: courses } })
+            .then(() => um.User.retrieve(tu))
+            .then((result) => expect(result.courses).toStrictEqual(courses))
+            .then(() => done());
+    });
 });
