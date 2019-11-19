b66320d3b1d1909ccf1bc2dd0e55e30fcba7deb9
szszs <shawn.zhou2@gmail.com>
2019-11-18 20:55:23 -0800

    Helping a user use case integration test

--- tests/integration/integration.test.js
+++ tests/integration/integration.test.js
@@ -89,7 +89,79 @@ describe("Posting new question test", () => {
                 expect(q).toBeTruthy();
                 expect(q.uuid).toBeTruthy();
                 expect(q.seeker).toBe(testUser);
+                expect(q.questionState).toBe("Waiting");
                 return done();
             })
+    });
+});
+
+
+describe("Helping a question test", () => {
+
+    let question;
+    const testSeeker = "testSeeker@example.com";
+    const testHelper = "testHelper@example.com";
+
+    afterAll(async () => {
+        const collection = await fastify.mongo.db.collection("questions");
+        const userCollection = await fastify.mongo.db.collection("users");
+        await collection.deleteOne({uuid: question.uuid});
+        await userCollection.deleteOne({ userId: testSeeker });
+        await userCollection.deleteOne({ userId: testHelper });
+    });
+
+    beforeAll(async () => {
+        const seekerUser = um.User.newUser(testSeeker);
+        await seekerUser.create();
+
+        const helperUser = um.User.newUser(testHelper);
+        await helperUser.create();
+    });
+
+    test("Offering help to a question test", async (done) => {
+        // step one: seeker posts the question
+        await fastify.inject({
+            method: "POST",
+            url: "/questions/create",
+            body: {
+                userId: testSeeker,
+                title: "Test Question",
+                courseCode: "CPEN 321",
+                questionText: "A test question."
+            }
+        });
+
+        // get the question ID
+        var seekerUser = await um.User.retrieve(testSeeker);
+        question = await qm.Question.retrieve(seekerUser.currentQuestion);
+
+        // step two: helper accepts to give help
+        const helpResponse = await fastify.inject({
+            method: "POST",
+            url: "/questions/accept",
+            body: {
+                userId: testHelper,
+                questionId: question.uuid,
+            }
+        });
+
+        expect(helpResponse.statusCode).toBe(200);
+
+        // check the seeker and helper status
+        seekerUser = await um.User.retrieve(testSeeker);
+        const helperUser = await um.User.retrieve(testHelper);
+
+        expect(seekerUser.currentQuestion).toBe(question.uuid);
+        expect(helperUser.currentQuestion).toBe(question.uuid);
+        
+        // check the question data
+        question = await qm.Question.retrieve(question.uuid);
+
+        expect(question.seeker).toBe(testSeeker);
+        expect(question.helperAccepted).toBe(true);
+        expect(question.finalHelper).toBe(testHelper);
+        expect(question.questionState).toBe("Matched");
+
+        done();
     });
 });
