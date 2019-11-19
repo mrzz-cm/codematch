af9cd215e51be5321e5b17aa86bef76fc3e2fbd8
John Ramsden <johnramsden@riseup.net>
2019-11-19 04:06:24 -0800

    Add android tools

--- scripts/android_before_install.sh
+++ scripts/android_before_install.sh
@@ -8,14 +8,10 @@ fi
 java -version
 update-alternatives --config java
 
-export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
-
 # download und unzip Android SDK Tools
 wget "https://dl.google.com/android/repository/$ANDROID_SDK_TOOLS"
 
 unzip -q "${ANDROID_SDK_TOOLS}" -d "${HOME}/sdk"
-export PATH="${PATH}:${HOME}/sdk/tools/bin"
-export ANDROID_HOME="${HOME}/sdk"
 
 # create empty cfg file to prevent sdkmanager warning message
 mkdir -p "${HOME}/.android" && touch "${HOME}/.android/repositories.cfg"
