686a6edb5bf644c3d9b8f11f371d807da4c1b983
John Ramsden <johnramsden@riseup.net>
2019-11-19 05:56:25 -0800

    Add user non functional requirement test

--- tests/requirements/match.test.js
+++ tests/requirements/match.test.js
@@ -36,11 +36,37 @@ describe("Check matching functional requirements", () => {
 
         logger.info("Time to Run matching", { time });
 
-        // API response time to input data under 30 ms
-        expect(time).toBeLessThan(30);
-
         // Matching  algorithm and database lookup under 100 ms
         expect(time).toBeLessThan(100);
+
+        done();
+    });
+});
+
+describe("Check user creation non-functional requirement", () => {
+
+    test("Run user creation in under 30 ms", async (done) => {
+        const t0 = performance.now();
+        const testUser = "nfrusertestuser@example.com";
+
+        await fastify.inject({
+            method: "POST",
+            url: "/user/register",
+            body: {
+                test_email: testUser,
+                longitude: 100,
+                latitude: -100,
+                access_token: "NOT_A_TOKEN"
+            }
+        });
+
+        const t1 = performance.now();
+        const time = t1 - t0;
+
+        logger.info("Time to run user creation", { time });
+
+        // API response time to input data under 30 ms
+        expect(time).toBeLessThan(30);
 
         done();
     });
