eacc9b062b4181860c6c3191e99e074781f631f6
John Ramsden <john@ubc.ramsdenj.ca>
2019-11-18 19:13:45 -0800

    Add travis manifest

---
+++ scripts/mongosetup.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+mongo admin --eval 'db.createUser(
+        {
+          user: "admin",
+          pwd: "password",
+          roles: [ 
+            { role: "userAdminAnyDatabase", db: "admin" }, 
+            "readWriteAnyDatabase"
+          ]
+        }
+      )'
+mongo codematch --eval 'db.createUser(
+      {
+        user: "codematch",
+        pwd: "password",
+        roles: [ "dbOwner" ]
+      }
+    )'
+mongorestore --db=codematch --username=codematch --password=password \
+    "$TRAVIS_BUILD_DIR/backend/data/tests/dump/codematch"
