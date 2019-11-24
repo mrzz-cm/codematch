18ce968a78cecd773cff7ab452069059b068fa5e
John Ramsden <john@ubc.ramsdenj.ca>
2019-11-24 15:22:10 -0800

    Comment Authentication module

--- authentication/index.js
+++ authentication/index.js
@@ -1,14 +1,13 @@
 const fastifyPlugin = require("fastify-plugin");
 const jwtPlugin = require("fastify-jwt");
 const oauthPlugin = require("fastify-oauth2");
-
 const req = require("request-promise-native");
 
 const config = require("../config");
 
-// https://github.com/fastify/fastify-jwt
-
-// Authentication preValidation plugin
+/**
+ * Authentication preValidation plugin
+ */
 const jwtValdatorPlugin = fastifyPlugin(async function(fastify) {
     fastify.register(jwtPlugin, {
         secret: config.jwtSecret
@@ -26,6 +25,12 @@ const jwtValdatorPlugin = fastifyPlugin(async function(fastify) {
     });
 });
 
+/**
+ * Request an email to verify token
+ *
+ * @param token
+ * @returns {Promise<Error>}
+ */
 async function requestEmail(token) {
     try {
         return await req({
@@ -42,8 +47,15 @@ async function requestEmail(token) {
     }
 }
 
-// Verifies that the provided request contains an authorization header
-// with a token that belongs to the provided user (identified by their userId).
+/**
+ * Verifies that the provided request contains an authorization header
+ * with a token that belongs to the provided user (identified by their userId).
+ *
+ * @param fastify Fastify instance
+ * @param request Fastify request object
+ * @param userId
+ * @returns {boolean}
+ */
 function verifyUserToken(fastify, request, userId) {
     // Dont require auth during testing
     if (process.env.MODE === "test") {return true;}
@@ -53,9 +65,10 @@ function verifyUserToken(fastify, request, userId) {
         return false;
     }
 
-    const decodedToken = fastify.jwt.decode(authHeader.replace(/Bearer\s+/, ""));
+    const decodedToken = fastify.jwt.decode(
+        authHeader.replace(/Bearer\s+/, ""));
     
-    return (decodedToken == userId);
+    return (decodedToken === userId);
 }
 
 // https://github.com/fastify/fastify-oauth2
