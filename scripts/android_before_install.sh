5f32c3126b9f531dabff998cfc56693741f73622
John Ramsden <johnramsden@riseup.net>
2019-11-25 23:03:13 -0800

    Exit on error, create sdk/

--- scripts/android_before_install.sh
+++ scripts/android_before_install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e
+
 if ! dpkg -s openjdk-8-jdk >/dev/null 2>&1; then
     apt-get update
     apt-get install openjdk-8-jdk android-tools-adb
@@ -9,9 +11,10 @@ java -version
 update-alternatives --config java
 
 # download und unzip Android SDK Tools
-wget "https://dl.google.com/android/repository/$ANDROID_SDK_TOOLS"
+wget "https://dl.google.com/android/repository/$ANDROID_SDK_TOOLS" >/dev/null 2>&1
 
-unzip -v -q "${ANDROID_SDK_TOOLS}" -d "${HOME}/sdk"
+mkdir "${HOME}/sdk"
+unzip "${ANDROID_SDK_TOOLS}" -d "${HOME}/sdk"
 
 # create empty cfg file to prevent sdkmanager warning message
 mkdir -p "${HOME}/.android" && touch "${HOME}/.android/repositories.cfg"
